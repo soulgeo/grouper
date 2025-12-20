ebb500fdd5b575adbfbdfdd247b873fb43833b30
George Soulantikas <soulgeo01@gmail.com>
2025-12-20 12:25:51 +0200

    core application

--- users/views.py
+++ users/views.py
@@ -1,16 +1,2 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
-
-
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/")
-    else:
-        form = UserCreationForm()
-    context = {
-        "form": form,
-    }
-    return render(request, "register.html", context)
