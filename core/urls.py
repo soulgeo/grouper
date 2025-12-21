3cd3f9211ca6037e99a5dff726e8933178087db9
George Soulantikas <soulgeo01@gmail.com>
2025-12-21 16:58:52 +0200

    contacts view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('contacts', views.contacts, name='contacts'),
     path('secret/', views.secret, name='secret'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Contact
+
 
 def index(request):
     return render(request, 'index.html')
@@ -9,3 +11,11 @@ def index(request):
 @login_required
 def secret(request):
     return render(request, 'secret.html')
+
+
+@login_required
+def contacts(request):
+    user = request.user
+    contacts = Contact.objects.filter(user=user)
+    context = {"contacts": contacts}
+    return render(request, 'contacts.html', context=context)
