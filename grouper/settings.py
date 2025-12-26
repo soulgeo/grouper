3ab386ede4e55d324b35baf3a30efb21effb0dd8
George Soulantikas <soulgeo01@gmail.com>
2025-12-26 18:43:33 +0200

    tailwind bugfixes, typography, widget-tweaks

--- grouper/settings.py
+++ grouper/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'django_countries',
+    'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
