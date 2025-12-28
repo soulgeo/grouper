a6d79acd8505f0d2b447905cb4eb1835efa34d88
George Soulantikas <soulgeo01@gmail.com>
2025-12-28 16:50:38 +0200

    add profile pic to user profile, make username unique

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.templatetags.static import static
 from django_countries.fields import CountryField
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100, blank=True)
@@ -19,9 +20,16 @@ class UserProfile(models.Model):
         User, on_delete=models.CASCADE, related_name='profile'
     )
     country = CountryField(blank=True)
+    image = models.ImageField(upload_to='profiles/', null=True, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     email_verified_at = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        return static('src/img/profile_default.png')
 
     def __str__(self) -> str:
         return f"{self.user.username}'s Profile"
