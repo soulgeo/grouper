2e25e5262352654f94344cf3774085c1a17e9972
George Soulantikas <soulgeo01@gmail.com>
2025-12-28 16:51:47 +0200

    make media displayable, change navbar look

--- grouper/urls.py
+++ grouper/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('_nested_admin/', include('nested_admin.urls')),
     path('', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
