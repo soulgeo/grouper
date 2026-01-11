185a61fc4b8690fcb31418fe982b2292828046e6
George Soulantikas <soulgeo01@gmail.com>
2026-01-11 20:37:23 +0200

    fix: redirect to chat app from friend button with correct chat opened

--- chat/views.py
+++ chat/views.py
@@ -15,8 +15,23 @@ def chat_home(request):
     user = request.user
     profiles = UserProfile.objects.filter(user__in_contacts_of__user=user)
 
+    room = None
+
+    target_user_id = request.GET.get('user_id')
+    if target_user_id:
+        contact_user = [User.objects.get(id=target_user_id)]
+        user = [user]
+        room, created = ChatRoom.objects.filter(
+            chat_type=ChatRoom.ChatType.CONTACTS, users__in=user
+        ).get_or_create(users__in=contact_user)
+        if created:
+            room_name = f"{user[0].username}_{contact_user[0].username}"
+            room.name = room_name
+            room.save()
+            room.users.set(user + contact_user)
+
     template = 'chat.html'
-    context = {'profiles': profiles}
+    context = {'profiles': profiles, 'room': room}
     return render(request, template, context)
 
 
